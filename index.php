f5f37650a2535e4bcd6fcb63726896e03f4eeaec
chmez <chmez070@gmail.com>
2023-01-25 01:48:16 +0200

    Process multiple rules per event

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php
 
-$settings = json_decode(file_get_contents('settings.json'), true);
+if (!empty($content = file_get_contents('php://input')))
+{
+	$pushed_subject = explode(PHP_EOL, current(json_decode($content, true)))[1];
+	$settings = json_decode(file_get_contents('settings.json'), true);
 
-$subject = explode(
-	PHP_EOL,
-	current(json_decode(file_get_contents('php://input'), true)),
-)[1];
-
-if (($event = array_search($subject, $settings['subjects'])) !== false) {
-	file_get_contents(sprintf(
-		'https://maker.ifttt.com/trigger/%s/with/key/%s',
-		$event,
-		$settings['key'],
-	));
+	foreach ($settings['subjects'] as $event => $subjects)
+		foreach ($subjects as $current_subject)
+			if ($current_subject === $pushed_subject)
+				file_get_contents(sprintf(
+					'https://maker.ifttt.com/trigger/%s/with/key/%s',
+					$event,
+					$settings['key'],
+				));
 }
